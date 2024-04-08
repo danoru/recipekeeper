9153f38e91727c8e7a562db5f78d0c5c3e7a4dcf
Daniel Mendez <awilddanoru@gmail.com>
2024-04-08 10:48:18 -0700

    feat: Add functionality for user favorites

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,16 +1,26 @@
 import Grid from "@mui/material/Grid";
 import Head from "next/head";
+import FavoriteCreators from "../../src/components/users/FavoriteCreators";
+import FavoriteRecipes from "../../src/components/users/FavoriteRecipes";
 import ProfileLinkBar from "../../src/components/users/ProfileLinkBar";
 import ProfileStatBar from "../../src/components/users/ProfileStatBar";
 import { getAllUsers } from "../../src/data/users";
-import { USER_LIST_TYPE } from "../../src/types";
+import {
+  CREATOR_LIST_TYPE,
+  RECIPE_LIST_TYPE,
+  USER_LIST_TYPE,
+} from "../../src/types";
+import { getAllCreators } from "../../src/data/creators";
+import { getAllRecipes } from "../../src/data/recipes";
 
 interface Props {
   user: USER_LIST_TYPE;
+  creators: CREATOR_LIST_TYPE[];
+  recipes: RECIPE_LIST_TYPE[];
 }
 
 function UserPage(props: Props) {
-  const { user } = props;
+  const { user, creators, recipes } = props;
   const title = `${user.profile.name}'s Profile • Savry`;
 
   return (
@@ -21,15 +31,11 @@ function UserPage(props: Props) {
       <Grid container>
         <ProfileStatBar />
         <ProfileLinkBar />
-        <Grid item xs={8}>
-          Favorite Creators
-        </Grid>
+        <FavoriteCreators creators={creators} />
         {/* <Grid item xs={4}>
           Cooklist
         </Grid> */}
-        <Grid item xs={8}>
-          Favorite Recipes
-        </Grid>
+        <FavoriteRecipes recipes={recipes} />
         {/* <Grid item xs={4}>
           Ratings
         </Grid>
@@ -69,8 +75,17 @@ export async function getStaticProps({ params }: any) {
   const { username } = params;
   const user = getAllUsers().find((user) => user.username === username);
 
+  const creators = getAllCreators().filter((creator) =>
+    user?.favorites?.creators.includes(creator.link)
+  );
+  const recipes = getAllRecipes().filter((recipe) =>
+    user?.favorites?.recipes.includes(recipe.name)
+  );
+
   return {
     props: {
+      creators,
+      recipes,
       user,
     },
     revalidate: 1800,
---
+++ src/components/users/FavoriteCreators.tsx
@@ -0,0 +1,90 @@
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Grid from "@mui/material/Grid";
+import Link from "next/link";
+import Typography from "@mui/material/Typography";
+
+import { CREATOR_LIST_TYPE } from "../../types";
+
+interface Props {
+  creators: CREATOR_LIST_TYPE[];
+}
+function FavoriteCreators(props: Props) {
+  const { creators } = props;
+
+  return (
+    <Grid container xs={8}>
+      <Grid
+        item
+        style={{
+          borderBottom: "1px solid black",
+          display: "flex",
+          justifyContent: "space-between",
+          lineHeight: "0",
+          margin: "0 auto",
+          width: "75%",
+        }}
+      >
+        <Typography variant="h6" component="div">
+          FAVORITE CREATORS
+        </Typography>
+      </Grid>
+      <Grid
+        container
+        item
+        rowSpacing={1}
+        columnSpacing={2}
+        sx={{
+          margin: "10px auto",
+          maxWidth: "75%",
+        }}
+      >
+        {creators.map((creator: any, i: number) => (
+          <CreatorCard
+            key={`card-${i}`}
+            name={creator.name}
+            link={`creators/${creator.link}`}
+            image={creator.image}
+            website={creator.website}
+            instagram={creator.instagram}
+            youtube={creator.youtube}
+            sx={{
+              width: "100%",
+              height: "100%",
+            }}
+          />
+        ))}
+      </Grid>
+    </Grid>
+  );
+}
+
+function CreatorCard(props: any) {
+  return (
+    <Grid item>
+      <Link href={props.link}>
+        <Card
+          sx={{
+            width: "200px",
+            height: "250px",
+            cursor: "pointer",
+          }}
+        >
+          <CardMedia
+            sx={{ height: 140 }}
+            image={props.image}
+            title={props.name}
+          />
+          <CardContent>
+            <Typography variant="h6" component="div">
+              {props.name}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Link>
+    </Grid>
+  );
+}
+
+export default FavoriteCreators;
