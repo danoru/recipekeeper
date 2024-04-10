dd4dbd7abd9c894d60b31debb65cfba785627dca
Daniel Mendez <awilddanoru@gmail.com>
2024-04-10 15:35:10 -0700

    feat: Add basic structure for activity page

---
+++ pages/activity/index.tsx
@@ -0,0 +1,80 @@
+import Add from "@mui/icons-material/Add";
+import Divider from "@mui/material/Divider";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import Grid from "@mui/material/Grid";
+import Head from "next/head";
+import Link from "@mui/material/Link";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import Stack from "@mui/material/Stack";
+import OutdoorGrillIcon from "@mui/icons-material/OutdoorGrill";
+import { getAllUsers } from "../../src/data/users";
+import { USER_LIST_TYPE } from "../../src/types";
+import ProfileStatBar from "../../src/components/users/ProfileStatBar";
+import ProfileLinkBar from "../../src/components/users/ProfileLinkBar";
+
+interface Props {
+  users: USER_LIST_TYPE[];
+}
+
+function Activity(props: Props) {
+  const { users } = props;
+  const user = users[0];
+  const title = `${users[0].username}'s Activity Stream`;
+  const diaryEntries = users[0].diary;
+  const avatarSize = "56px";
+
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <Grid container>
+        <Grid item />
+        <ProfileStatBar
+          avatarSize={avatarSize}
+          username={user.username}
+          name={user.profile.name}
+        />
+        <ProfileLinkBar username={user.username} />
+      </Grid>
+      <Grid item xs={8} sx={{ marginTop: "10px" }}>
+        <Stack
+          spacing={1}
+          divider={<Divider orientation="horizontal" flexItem />}
+        >
+          {diaryEntries?.map((diaryEntry, i) => (
+            <Stack key={i} direction="row" sx={{ justifyContent: "center" }}>
+              <div>
+                <Link href={`/${user.username}`} underline="none">
+                  {user.username}
+                </Link>{" "}
+                made{" "}
+                <Link
+                  href={`/recipe/${diaryEntry.recipe
+                    .toLowerCase()
+                    .replace(/ /g, "-")}`}
+                >
+                  {diaryEntry.recipe}
+                </Link>{" "}
+                on {diaryEntry.date}
+              </div>
+            </Stack>
+          ))}
+        </Stack>
+      </Grid>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const users = getAllUsers();
+
+  return {
+    props: {
+      users,
+    },
+    revalidate: 1800,
+  };
+}
+
+export default Activity;
