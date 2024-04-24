f7bbaf36e0d94e6e7a80ca5b6f45abe0687df7a6
Daniel Mendez <awilddanoru@gmail.com>
2024-04-24 12:13:42 -0700

    fix: Fixes issue with TypeScript wrongfully stating color is undefined

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,14 +1,19 @@
 import { createTheme } from "@mui/material/styles";
-import { ThemeOptions } from "@mui/material/styles";
 
-export const theme: ThemeOptions = createTheme({
+export const theme = createTheme({
   palette: {
     mode: "dark",
     primary: {
       main: "#66fcf1",
+      light: "rgb(132, 252, 243",
+      dark: "rgb(71, 176, 168",
+      contrastText: "rgba(0,0,0,0.87)",
     },
     secondary: {
       main: "#45a29e",
+      light: "rgb(106,180,177",
+      dark: "rgb(48,113,110)",
+      contrastText: "#fff",
     },
     background: {
       default: "#0b0c10",
@@ -17,6 +22,7 @@ export const theme: ThemeOptions = createTheme({
     text: {
       primary: "#c5c6c7",
       secondary: "rgba(255,255,255,0.7)",
+      disabled: "rgba(255, 255, 255, 0.5)",
     },
   },
 });
