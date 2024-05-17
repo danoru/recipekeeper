48c4292ca284b3b6cce8bfbe989241891da148ff
Daniel Mendez <awilddanoru@gmail.com>
2024-05-17 08:01:54 -0700

    fix: Fixes unescaped entity

--- src/components/home/LoggedInHomePage.tsx
+++ src/components/home/LoggedInHomePage.tsx
@@ -1,11 +1,16 @@
+// import FriendRecipeActivity from "./FriendRecipeActivity";
+// import PopularRecipeActivity from "./PopularRecipeActivity";
 import Typography from "@mui/material/Typography";
 
 function LoggedInHomePage({ username }: any) {
   return (
     <main style={{ textAlign: "center" }}>
       <Typography variant="h6">
-        Welcome back, {username}. Here's what your friends have been cooking...
+        Welcome back, {username}. Here&apos;s what your friends have been
+        cooking...
       </Typography>
+      {/* <FriendRecipeActivity />
+      <PopularRecipeActivity /> */}
     </main>
   );
 }
