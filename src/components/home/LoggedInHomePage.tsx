212a28591442eabd8d99c42b7dd9763bfc8365a1
Daniel Mendez <awilddanoru@gmail.com>
2024-05-17 20:07:04 -0700

    feat: Adds link to profile from main page

--- src/components/home/LoggedInHomePage.tsx
+++ src/components/home/LoggedInHomePage.tsx
@@ -2,6 +2,7 @@ import FriendRecipeActivity from "./FriendRecipeActivity";
 // import PopularRecipeActivity from "./PopularRecipeActivity";
 import Typography from "@mui/material/Typography";
 import { RECIPE_LIST_TYPE, UserDiary } from "../../types";
+import Link from "@mui/material/Link";
 
 interface Props {
   recentEntries: UserDiary[];
@@ -13,8 +14,8 @@ function LoggedInHomePage({ recentEntries, recipes, username }: Props) {
   return (
     <main style={{ marginTop: "20px", textAlign: "center" }}>
       <Typography variant="h6">
-        Welcome back, {username}. Here&apos;s what your friends have been
-        cooking...
+        Welcome back, <Link href={`/${username}`}>{username}</Link>. Here&apos;s
+        what your friends have been cooking...
       </Typography>
       <FriendRecipeActivity recentEntries={recentEntries} recipes={recipes} />
       {/* <PopularRecipeActivity /> */}
